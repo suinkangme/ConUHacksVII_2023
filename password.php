b5e48fff4576c4247b1b4d66e77ea22338b40663
Mikoos83 <107123788+Mikoos83@users.noreply.github.com>
2023-01-21 19:14:51 -0500

    Revert "Create password.php"
    This reverts commit 19448ffee8a2938be7b8f5a74f76709294ffee73.

--- password.php
+++ password.php
@@ -13,66 +13,83 @@
    <h1>Welcome to Password Generator</h1>
    <form method = POST>
      <div>
-       <p>
-         Please enter the name of the animal in the following image.
-         Caution: Vowels must be capitalized. </p>
+       <p>Please enter the name of the animal in the following image.</p>
          <?php
-          $key = rand (0, 11);
-          switch($key){
+          $key1 = rand (0, 11);
+          switch($key1){
             case 0:
-              echo "<img src = \"img/0.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/0.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 1:
-              echo "<img src = \"img/1.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/1.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 2:
-              echo "<img src = \"img/2.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/2.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 3:
-              echo "<img src = \"img/3.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/3.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 4:
-              echo "<img src = \"img/4.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/4.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 5:
-              echo "<img src = \"img/5.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/5.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 6:
-              echo "<img src = \"img/6.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/6.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 7:
-              echo "<img src = \"img/7.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/7.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 8:
-              echo "<img src = \"img/8.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/8.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 9:
-              echo "<img src = \"img/9.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/9.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 10:
-              echo "<img src = \"img/10.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/10.jpg\" width =\"400\" height = \"400\"\">";
               break;
             case 11:
-              echo "<img src = \"img/11.jpg\" width =\"400\" height = \"400\"\">";
+              echo "<img src = \"img/animal/11.jpg\" width =\"400\" height = \"400\"\">";
               break;
           }
+          echo "<br/>"
          ?>
-
          <input type = text />
      </div>
      <div>
-       <p>
-         Please enter the name of the color in the following image.
-         Caution: Vowels must be capitalized.
-         <?php echo rand(0, 8)?>
-       </p>
+       <p>Please enter the name of the color in the following image.</p>
+        <?php $key2 = rand(0, 7);
+          switch($key2){
+            case 0:
+              echo "<img src = \"img/color/0.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 1:
+              echo "<img src = \"img/color/1.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 2:
+              echo "<img src = \"img/color/2.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 3:
+              echo "<img src = \"img/color/3.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 4:
+              echo "<img src = \"img/color/4.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 5:
+              echo "<img src = \"img/color/5.png\" width =\"400\" height = \"400\"\">";
+              break;
+            case 6:
+              // echo "<img src = \"img/color/6.jpg\" width =\"400\" height = \"400\"\">";
+              // break;
+          }
+          echo "<br/>"
+        ?>
        <input type = text />
      </div>
      <div>
-       <p>
-         Please choose one of the word in the table.
-         Caution: Vowels must be capitalized.
-       </p>
+       <p>Please choose one of the word in the table.</p>
        <table>
          <tr>
            <td>happy</td>
@@ -95,8 +112,7 @@
        <p>
           Please enter 2-digit random number (00 ~ 99)
        </p>
-        <input type = text / id="numb">
-        <button type="button" onclick="myFunction()">Submit</button>
+        <input type = text />
      </div>
      <div>
        <p>
@@ -104,26 +120,11 @@
        </p>
  <input type = text />
      </div>
-     <button type="button" onclick="myFunction()">Submit</button>
-    
-<p id="demo"></p>
-
+     <button>Submit</button>
    </form>
  </body>
-  
 <sript>
-  function myFunction() {
-      let x = document.getElementById("numb").value;
- 
-      let text;
-      if (isNaN(x) || x > 100) {
-         text = "Input not valid";
-      }else {
-         text = "Input OK";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
- 
+
 </sript>
 
  </html>
